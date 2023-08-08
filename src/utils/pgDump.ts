55a6904288df4e2e5e6494064cd29ee665c0e16f
Tyson <42539846+Techguy9078@users.noreply.github.com>
2023-08-08 19:06:17 +0800

    Fixed Dumping All Books

--- src/utils/pgDump.ts
+++ src/utils/pgDump.ts
@@ -2,17 +2,14 @@
 import { execa } from "execa";
 
 /**
- * Dumps all postgresql database to .sql file in ./backups/
+ * Dumps all postgresql database to .csv file in ./backups/
  * @returns Success or returns Error
  */
 export default async function PGDUMP(): Promise<Error | "Success!"> {
 	const path = process.env.DATABASE_URL;
 
 	if (!path) return Error("No DATABASE_URL environment variable.");
-	// await stdoutData(path);
-	const thisCSV = await csvOutput(path);
-	console.log(thisCSV);
-	// await stdoutSchema(path);
+	await csvOutput(path);
 
 	return "Success!";
 }
@@ -20,47 +17,11 @@ export default async function PGDUMP(): Promise<Error | "Success!"> {
 async function csvOutput(path: string) {
 	const { stdout } = await execa(`D:\\Databases\\pgAdmin 4\\runtime\\psql`, [
 		`--dbname=${path}`,
+		`--command=SELECT * FROM public."ScannedBook"`,
 		"--csv",
 		`--output=./backups/${new Date()
 			.toLocaleDateString()
-			.replaceAll("/", "_")}_data_only.csv`,
-	]);
-
-	return stdout;
-}
-
-async function stdoutData(path: string) {
-	const { stdout } = await execa(`D:\\Databases\\pgAdmin 4\\runtime\\pg_dump`, [
-		`--dbname=${path}`,
-		//  different formats can be 'plain', 'custom', 'directory', 'tar',
-		`--format=plain`,
-		`--data-only`,
-		`--quote-all-identifiers`,
-		`--no-acl`,
-		`--no-owner`,
-		// File to write sql to
-		`--file=./backups/${new Date()
-			.toLocaleDateString()
-			.replaceAll("/", "_")}_data_only.csv`,
-	]);
-
-	return stdout;
-}
-
-async function stdoutSchema(path: string) {
-	const { stdout } = await execa(`D:\\Databases\\pgAdmin 4\\runtime\\pg_dump`, [
-		`--dbname=${path}`,
-		//  different formats can be 'plain', 'custom', 'directory', 'tar',
-		`--format=plain`,
-		`--inserts`,
-		`--schema-only`,
-		`--quote-all-identifiers`,
-		`--no-acl`,
-		`--no-owner`,
-		// File to write sql to
-		`--file=./backups/${new Date()
-			.toLocaleDateString()
-			.replaceAll("/", "_")}_schema_only.sql`,
+			.replaceAll("/", "_")}_backup.csv`,
 	]);
 
 	return stdout;
