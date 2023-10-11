9753dbdda2563f8cbc0289dc7f5a5d87d5ca4a3b
Tyson <42539846+Techguy9078@users.noreply.github.com>
2023-10-11 15:01:01 +0800

    Fixes

--- src/app/Admin/page.tsx
+++ src/app/Admin/page.tsx
@@ -1,24 +1,33 @@
 "use client";
 import { Box, Button, Text, VStack, useColorModeValue } from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function Admin() {
-	return (
-		<Box
-			p={4}
-			bgColor={useColorModeValue("gray.200", "gray.700")}
-			rounded={"md"}
-		>
-			<VStack spacing={4} align={"left"} pb={4}>
-				<Text fontSize="2xl">Admin Functions</Text>
-				<Button
-					bgColor={"green.200"}
-					onClick={() => {
-						import("@/utils/pgDump").then((module: any) => module.default());
-					}}
-				>
-					Export Database
-				</Button>
-			</VStack>
-		</Box>
-	);
+  const [exported, setExported] = useState<boolean>(false);
+  return (
+    <Box
+      p={4}
+      bgColor={useColorModeValue("gray.200", "gray.700")}
+      rounded={"md"}
+    >
+      <VStack spacing={4} align={"left"} pb={4}>
+        <Text fontSize="2xl">Admin Functions</Text>
+        {exported && <Text>Exported Database!</Text>}
+        <Button
+          variant={"solid"}
+          color={"black"}
+          onClick={() => {
+            setExported(false);
+            import("@/utils/pgDump").then((module: any) =>
+              module.default().then((result: any) => {
+                console.log(result), setExported(true);
+              })
+            );
+          }}
+        >
+          Export Database
+        </Button>
+      </VStack>
+    </Box>
+  );
 }
--- src/utils/pgDump.ts
+++ src/utils/pgDump.ts
@@ -6,24 +6,29 @@ import { execa } from "execa";
  * @returns Success or returns Error
  */
 
-export default async function PGDUMP(): Promise<Error | "Success!"> {
-	const path = process.env.DATABASE_URL;
+export default async function PGDUMP(): Promise<Error | string> {
+  const path = process.env.DATABASE_URL;
 
-	if (!path) return Error("No DATABASE_URL environment variable.");
-	await csvOutput(path);
+  if (!path) return Error("No DATABASE_URL environment variable.");
 
-	return "Success!";
+  const stdout = await csvOutput(path);
+  return stdout;
 }
 
 async function csvOutput(path: string) {
-	const { stdout } = await execa(`${process.env.PGADMIN_PATH}`, [
-		`--dbname=${path}`,
-		`--command=SELECT * FROM public."ScannedBook"`,
-		"--csv",
-		`--output=${process.env.BACKUP_PATH}${new Date()
-			.toLocaleDateString()
-			.replaceAll("/", "_")}_backup.csv`,
-	]);
+  try {
+    const { stdout } = await execa(`${process.env.PGADMIN_PATH}`, [
+      `--dbname=${path}`,
+	  `--tuples-only`,
+      `--command=SELECT * FROM public."ScannedBook"`,
+      "--csv",
+      `--output=${process.env.BACKUP_PATH}${new Date()
+        .toLocaleDateString()
+        .replaceAll("/", "_")}_backup.csv`,
+    ]);
 
-	return stdout;
+    return stdout;
+  } catch (error: any) {
+    return error;
+  }
 }
