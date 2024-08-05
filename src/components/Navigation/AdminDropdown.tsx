d0aeb33ba9ea8d028adbc4a03bcdae86b84f1410
David <davidsatwork2@hotmail.com>
2024-08-05 11:54:47 +0800

    adds admin mode to navbar

---
+++ src/components/Navigation/AdminDropdown.tsx
@@ -0,0 +1,62 @@
+import { useIsMobile } from "@/utils/isMobile";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
+import Link from "next/link";
+import React from "react";
+
+const AdminDropdown = () => {
+  const isMobile = useIsMobile();
+
+  const adminLinks = [
+    {
+      name: "Reports",
+      href: "Reports",
+      color: { light: "#836a8a", dark: "#65466D" },
+    },
+    {
+      name: "Oldest",
+      href: "Oldest",
+      color: { light: "yellow.800", dark: "yellow.700" },
+    },
+    {
+      name: "Stats",
+      href: "Stats",
+      color: { light: "#ae9991", dark: "#86665a" },
+    },
+    {
+      name: "Admin",
+      href: "Admin",
+      color: { light: "purple.600", dark: "purple.500" },
+    },
+  ];
+
+  return (
+    <Menu>
+      <MenuButton
+        as={Button}
+        rightIcon={<ChevronDownIcon />}
+        py={4}
+        px={3}
+        my={1}
+        display={"flex"}
+        h={"100%"}
+        width={isMobile ? "100%" : "135px"}
+        alignItems={"center"}
+        rounded={"none"}
+        bgColor={"red !important"}
+        color={"white"}
+      >
+        Boss Mode
+      </MenuButton>
+      <MenuList>
+        {adminLinks.map((link) => (
+          <MenuItem as={Link} href={link.href} key={link.name}>
+            {link.name}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default AdminDropdown;
